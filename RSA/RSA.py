5d179369498bbd5cba2ff410f385e2f342afa8a1
Revaz Bakuradze <rezibakuradze@gmail.com>
2022-03-16 23:27:17 -0400

    RSA - able to generate public and private keys

--- RSA/RSA.py
+++ RSA/RSA.py
@@ -1,28 +1,35 @@
-from xmlrpc.client import ServerProxy
+'''
+Thank you Aleksandr Molchagin for helping me writing keys in the file! 
+you are the legenda
+'''
+
+
+import os
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
 def generate_keys():
-    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    with open("keys/private_key.pem", "wb+") as private_key_file_obj:
+        with open("keys/public_key.pem", "wb+") as public_key_file_obj:
 
-    with open("private_key.pem", "rb") as private_key_file:
-        private_key_file = serialization.load_pem_private_key(private_key_file.read(), password=None)
+            private_key = rsa.generate_private_key(
+                                public_exponent=65537,
+                                key_size=2048,
+                            )
+            public_key = private_key.public_key()
 
-    # pem = private_key.private_bytes(
-    #     encoding=serialization.Encoding.PEM,
-    #     format=serialization.PrivateFormat.PKCS8,
-    #     encryption_algorithm=serialization.BestAvailableEncryption(b'mypassword') # serialization.NoEncryption
-    # )
+            private_key_bytes = private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption()
+            )
 
-    public_key = private_key.public_key()
-    pem = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
+            private_key_file_obj.write(private_key_bytes)
+            public_key_bytes = public_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo
+            )
+            public_key_file_obj.write(public_key_bytes)
 
-    with open("public_key.pem", "rb") as public_key_file:
-        public_key_file = serialization.load_pem_private_key(public_key_file.read(), password=None)
-
-    # pem.splitlines()[0]
-
+# Generate Public and Private Keys for CTF
 generate_keys()
